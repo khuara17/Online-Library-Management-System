a862d2438c2a0f32c1059036426a9c7b06a65cc0
vishal kamlapure <vishalkamlapure.vbak@gmail.com>
2019-09-01 13:26:53 +0530

    First commit

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2019 Online Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : Vishu2408 </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2019 Online Library Management System |<a href="https://google.com/" target="_blank"  > Designed by : Us</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
